e8461211a9b8fa9c0a95c51a1f136aad0fab1148
MalenaFG <malena.f.giusti@gmail.com>
2024-08-26 17:51:16 +0200

    new user endpoint

--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -12,4 +12,16 @@ router.post('/image', uploaderMiddleware.single('imageData'), (req, res) => {
     res.json({ cloudinary_url: req.file.path })
 })
 
+router.post('/images', uploaderMiddleware.array('imagesData'), (req, res) => {
+
+    if (!req.files) {
+        res.status(500).json({ errorMessage: 'Error caragndo los archivos' })
+        return
+    }
+
+    const images = req.files.map(elm => elm.path)
+
+    res.json({ cloudinary_urls: images })
+})
+
 module.exports = router
